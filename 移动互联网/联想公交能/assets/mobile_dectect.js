39ed20c4c040ae70f8f1a4b938b017507b48bd12
kuanglijun <kuanglijun@zmaitech.com>
2015-03-09 15:20:07 +0800

    klj

--- 移动互联网/联想公交能/assets/mobile_dectect.js
+++ 移动互联网/联想公交能/assets/mobile_dectect.js
@@ -2,7 +2,7 @@
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "//hm.baidu.com/hm.js?34edaff3d727ff8d31c355653eb9f713";
+  hm.src = "http://hm.baidu.com/hm.js?34edaff3d727ff8d31c355653eb9f713";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
